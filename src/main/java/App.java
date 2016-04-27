61dd89b2a941f8064ab388c0ae12b81c071a7c08
Andrew Russo and DJ Roychoudhury <dj@>
2016-04-27 11:12:06 -0700

    add individual task description page and generic not found message if no task. test passes.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -29,6 +29,13 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/tasks/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Task task = Task.find(Integer.parseInt(request.params(":id")));
+      model.put("task", task);
+      model.put("template", "templates/task.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
     post("/tasks", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
