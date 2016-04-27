d4b2d4452509ec02b3d2559eae753daa70ce4f11
Andrew Russo and DJ Roychoudhury <dj@>
2016-04-27 09:09:54 -0700

    pass test to make sure all instances of task return true.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,15 +1,18 @@
+import java.util.ArrayList;
 import java.time.LocalDateTime;
 
 public class Task{
   private String mDescription;
   private boolean mCompleted;
   private LocalDateTime mCreatedAt;
+  private static ArrayList<Task> instances = new ArrayList<Task>();
 
 
   public Task(String description) {
     mDescription = description;
     mCompleted = false;
     mCreatedAt = LocalDateTime.now();
+    instances.add(this);
   }
 
   public String getDescription() {
@@ -22,6 +25,10 @@ public class Task{
 
   public LocalDateTime getCreatedAt() {
     return mCreatedAt;
+  }
+
+  public static ArrayList<Task> all() {
+    return instances;
   }
 
 }
