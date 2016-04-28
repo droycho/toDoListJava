c9aa572dca974e0158a21c7862c6a795f06aa0f0
DJ Roychoudhury and Kevin Deganos <dj@>
2016-04-28 08:29:41 -0700

    pass first category instatiation test.

---
+++ src/test/java/CategoryTest.java
@@ -0,0 +1,16 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+  @After
+  public void tearDown() {
+    Category.clear();
+    Task.clear();
+  }
+
+  @Test
+  public void category_instatiatesCorretly_true() {
+    Category testCategory = new Category("Home");
+    assertEquals(true, testCategory instanceof Category);
+  }
+}
